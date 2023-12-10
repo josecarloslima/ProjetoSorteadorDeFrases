e9bde7fc40ac6677ce23f61cfaea5acbdf23b1b9
José Carlos Lima <josecarlosrlima@gmail.com>
2023-12-10 13:10:51 -0300

    alterações

--- frases.js
+++ frases.js
@@ -1,15 +1,79 @@
 var frases = [
     {
-      categoria: "Provérbio",
-      titulo: "Quem com ferro fere, com ferro será ferido",
-      texto: "Essa frase destaca a reciprocidade das ações, indicando que quem pratica o mal acabará por sofrer as consequências."
+      categoria: "MacGuffin",
+      titulo: "Vivendo Perigosamente",
+      texto: "Adicione o GLITCH DIE ao teste /combate."
     },
     {
-      categoria: "Motivação",
-      titulo: "O sucesso é a soma de pequenos esforços repetidos dia após dia",
-      texto: "Destaca a importância da consistência e do esforço contínuo na busca pelo sucesso."
+      categoria: "Interrupção",
+      titulo: "Carteirada",
+      texto: "Decida seu momento de agir na INICIATIVA."
     },
-    // Adicione mais frases conforme necessário
+    {
+      categoria: "MacGuffin",
+      titulo: "Just in Time",
+      texto: "Execute DUAS ações de movimento para perto de seu alvo, ou para longe dele."
+    },
+    {
+      categoria: "Edição de Cena",
+      titulo: "Presente Surpresa",
+      texto: "Adicione à cena um grupo de guardas /seguranças iual ao numero de participantes da mesa."
+    },
+    {
+      categoria: "MacGuffin",
+      titulo: "Recuperando o Fôlego",
+      texto: "Recupere um ponto de dano. Escolha se do tipo S ou P."
+    },
+    {
+      categoria: "Edição de Cena",
+      titulo: "Tela Azul",
+      texto: "Um equipamento eletrônico presente na cena apresenta um erro fatal e reinicia, perdendo assim o próximo turno de ação. No próximo turno o equipamento eletrônico escolhido não funciona."
+    },
+    {
+      categoria: "Edição de Cena",
+      titulo: "Click!Click!Click!",
+      texto: "Uma arma de fogo presente na cena esgotou sua munição e precisa ser recarregada, perdendo assim o próximo turno de ação. No próximo turno a arma de fogo escolhida não dispara."
+    },
+    {
+      categoria: "Edição de Cena",
+      titulo: "Isso Nunca me Aconteceu Antes...",
+      texto: "Um equipamento cibernético (AMP) presente na cena apresenta uma disfunção e reinicia, perdendo assim o próximo turno de ação. No próximo turno o equipamento cibernético (AMP) escolhido não funciona."
+    },
+    {
+      categoria: "Interrupção",
+      titulo: "Deixa Comigo",
+      texto: "Troque de lugar com um personagem que está sendo alvo de um ataques neste turno. Você usa sua defesa e se for atingido recebe o dano."
+    },
+    {
+      categoria: "MacGuffin",
+      titulo: "Oferecendo a Outra Face",
+      texto: "Depois de receber um dano você pode usar esta carta. Você automaticamente faz um ataque extra contra seu oponente. O ataque procede normalmente, mas não conta como uma ação, é apenas uma retribuição! Normal numa sexta-feira à noite!"
+    },
+    {
+      categoria: "Rebuliço",
+      titulo: "Paguem a Moça!",
+      texto: "Você baixa esta carta e todos os presentes na mesa devem baixar uma carta também, a escolha de cada um, e sem efeito. As vezes um pouco de caos é só um prouco de caos mesmo."
+    },
+    {
+      categoria: "Interrupção",
+      titulo: "Na Mosca!",
+      texto: "Ao atacar um oponente a PROTEÇÃO dele (Armor) não funcionará, fazendo com que seu dano vá direto para o Monitor dele. Em outras palavras neste ataque você impede a proteção de seu oponente."
+    },
+    {
+      categoria: "Rebuliço",
+      titulo: "Escravos de Jó",
+      texto: "Todos os particpantes da mesa, que tiverem cartas nas mãos, vão passar uma carta aleatória para o particpante sentado à direita deles. Quem não tem carta fica de fora."
+    },
+    {
+      categoria: "Edição de Cena",
+      titulo: "Jogada Ensaiada",
+      texto: "Escolha dois personagens que estejam na mesma cena, um deles pode ser o seu ou aliados do seu personagem. E os faça trocarem de oponentes. Mesmo que um tenha acado de derrotar um oponente, a troca ainda é válida."
+    },
+    {
+      categoria: "Edição de Cena",
+      titulo: "Isso Fazia Parte do Plano?!",
+      texto: "Escolha um meta-tipo que tem mais de um representante na cena em questão. Um golpe de sorte os atinge e no próximo teste /combate que fizerem recebem um bônus de situação de +1d."
+    },
   ];
   
   
