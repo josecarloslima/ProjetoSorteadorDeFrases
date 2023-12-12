7210e9d69761f3e8a454a98f9b023a2c9b2dfcf4
José Carlos Lima <josecarlosrlima@gmail.com>
2023-12-12 19:00:43 -0300

    Update frases.js

--- frases.js
+++ frases.js
@@ -67,19 +67,118 @@ var frases = [
     {
       categoria: "MacGuffin",
       titulo: "O GPS mandou virar",
-      texto: "Escolha dois personagens aliados e ativos na cena e troque-os de lugar (ou oponente que estejam enfrentando). Um dos personagens pode ser o seu."
+      texto: "Escolha dois personagens ativos na cena. Agora troque-os de posição com um assumindo a posição do outro. Um dos personagens pode ser o seu. Os personagens podem ser aliados ou oponentes, mas os dois devem ser do mesmo tipo (dois PCs, dois aliados ou dois oponentes)."
     },
     {
       categoria: "Plot Twist",
       titulo: "Isso não estava nos planos",
-      texto: "Escolha dois personagens oponentes e ativos na cena e troque-os de lugar (ou quem estão enfrentando). Ou você pode pegar dois oponentes ativos na cena e colocar para enfrentar um único personagem (que pode ser o seu). Se escolher esta opção e os Monitores dos oponentes estiverem iguais, ou melhores do que o do seu Personagem, você ganha um ponto de karma bônus no final da sessão."
+      texto: "Escolha dois personagens oponentes e ativos na cena. Agora troque-os de posição fazendo-os enfrentar um único personagem aliado. Que pode ou não ser o seu personagem. Por fim compare o pior Monitor de cada um dos lados; se o menor Monitor do aliado, ou de seu personagem, for menor do que os Monitores dos oponentes (um ou os dois), ganhe um ponto extra de Karma no fim da sessão e compre uma carta agora. Caso contrário apenas compre uma carta agora. "
     },
     {
       categoria: "MacGuffin",
       titulo: "Você no topo da prateleira!",
-      texto: "Escolha um personagem na cena e recupera uma quantidade de pontos de dano igual ao número de cartas que você descartar depois de ativar esta carta. Você escolhe o tipo de ponto recuperado, se S ou P. Você pode descartar quantas cartas quiser e tiver em mãos."
+      texto: "Descarte quantas cartas quiser e puder de sua mão além desta. Cada carta descartada recupera um ponto de dano de seu pior Monitor."
     },
-    
+    {
+      categoria: "MacGuffin",
+      titulo: "Está chovendo balas!",
+      texto: "Você dispara uma quantidade obscena de balas em seu oponente, adicione um ponto extra de dano (do mesmo tipo que sua arma de fogo) se causar dano em seu oponente."
+    },
+    {
+      categoria: "MacGuffin",
+      titulo: "Pau para toda obra",
+      texto: "Você arremata seu oponente com tantos golpes de uma vez que é difícil acompanhar com os olhos, adicione um ponto extra de dano (do mesmo tipo da arma de mão, arremesso ou luta desarmada que está usando) se causar dano em seu oponente."
+    },
+    {
+      categoria: "MacGuffin",
+      titulo: "Do Astral, com amor",
+      texto: "Você dispara seu feitiço com a potência máxima de sua existência mágica, adicione um ponto extra de dano (do mesmo tipo que seu feitiço) se causar dano em seu oponente."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "O destino de todos é compartilhado por cada um",
+      texto: "Ao baixar esta carta compare os Monitores de cada personagem aliado que esteja em cena e ativo. Você escolhe o tipo de Monitor, se S ou P. Aquele(s) que tiver(em) em melhor condição recebem um ponto de dano naquele Monitor. Aquele que receber este dano, compra uma carta."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Nós treinamos para isso",
+      texto: "Ao baixar esta carta você pode descartar até mais três outras cartas de sua mão. Cada carta que descartar vale um ponto bônus de Karma."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Granada",
+      texto: "Escolha dois aliados para tomar um ponto de dano cada, você escolhe o tipo. Os oponentes que eles estão enfrentando e mais dois oponentes adjacentes aos seus oponentes recebem dois pontos de dano cada indo direto para o Monitor deles. Ignorando possiveis proteções (Armor e cover). Os aliados atingidos compram uma carta cada."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Preciso de uma mão aqui",
+      texto: "Escolha um aliado que esteja enfrentando um oponente. No próximo round se ele se recusar a atacar seu oponente (ficando apenas na defesa), vocês dois compram uma carta."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Jogo sujo",
+      texto: "Baixe esta carta e escolha um oponente que esteja ativo na cena. Se ele der dano em algum aliado, ou em seu personagem, ele vai recuperar uma quantidade de pontos de dano no seu pior Monitor igual aos pontos de dano que conferiu. Você rceberá esta quantidade em cartas e um ponto extra de Karma."
+    },
+    {
+      categoria: "MacGuffin",
+      titulo: "Combate focado",
+      texto: "Seu oponente não pode atacar outro alvo que não seja você. Ele está trancado em você ignorando qualquer outra opção de ataque. A única forma dele ignorar você em um ataque é se ele for atingido pelo efeito de outra carta."
+    },
+    {
+      categoria: "Interrupção",
+      titulo: "Jogada de mestre",
+      texto: "Ao terminar seu turno, antes de passar a vez. Você age de novo. Podendo fazer novamente o que quiser, inclusive baixando uma nova carta. Mas atenção se você tiver duas destas cartas não pode as jogar em seguida, mas pode usar em rounds diferentes. Não fique bravo comigo, eu só faço as regras, ok!"
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Um minuto insuportável",
+      texto: "Ao baixar esta carta você pode baixar duas outras cartas do tipo Plot Twist que tenha e somar os dois efeitos. Além dos efeitos acumulados das duas cartas você receberá as benesses delas e mais um ponto bônus de Karma."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Você só pode estar brincando",
+      texto: "No próximo round todos os oponentes que seu personagem e seus aliados estão enfrentando receberão um dado extra no ataque, +1d, e vão curar um ponto de dano em seu pior Monitor. Você compra uma carta."
+    },
+    {
+      categoria: "Rebuliço",
+      titulo: "Vasculhando as oportunidades",
+      texto: "Cada participante da mesa pega uma carta aleatória de sua mão e passa para o particpante à direita. Quem não tem carta não participa."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Rei morto, rei posto",
+      texto: "Todos os personagens humanos ativos na cena recebem mais um dado, +1d, em seu próximo teste /combate. Você compra uma carta."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Os primeiros a acordar",
+      texto: "Todos os personagens Elfos e Anões ativos na cena recebem mais um dado, +1d, em seu próximo teste /combate. Você compra uma carta."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Nação Goblin",
+      texto: "Todos os personagens Troll e Orcs ativos na cena recebem mais um dado, +1d, em seu próximo teste /combate. Você compra uma carta."
+    },
+    {
+      categoria: "Rebuliço",
+      titulo: "F.U.B.A.R.",
+      texto: "Cada Jogador que tiver mais do que três cartas na mão, é obrigado a descartar (sem evocar efeito algum) até ficar com apenas três cartas na mão. O GM descarta faz a mesma coisa, mas se tiver mais do que 6 cartas na mão; ele descarta até ficar apenas com seis cartas na mão."
+    },
+    {
+      categoria: "Interrupção",
+      titulo: "Hoje não Satã",
+      texto: "Ao atingir o nível Staggered, K.I.A, ou K.O. Use esta carta e recupere os pontos de dano suficiente para você recuar para um ponto antes de Staggered."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "Chummers",
+      texto: "Todos os personagens humanos ativos na cena recebem mais um dado, +1d, em seu próximo teste /combate. Você compra uma carta."
+    },
+    {
+      categoria: "Plot Twist",
+      titulo: "A hora da humanidade",
+      texto: "Todos os personagens humanos ativos na cena recebem um ponto de dano a menos em seu pior Monitor a menos que descartem uma carta. Os Jogadores gastam por seus personagens, o GM faz um gasto pelo conjunto de NPCs aliados dos PCs presentes na cena, e outro pelos oponentes dos PCs."
+    },
   ];
   
   
