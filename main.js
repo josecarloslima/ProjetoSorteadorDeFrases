91bd0f6698f06790a63eb32954c3b968f1a8739c
José Carlos Lima <josecarlosrlima@gmail.com>
2023-12-13 18:00:55 -0300

    modificações

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-function embaralharFrases() {
-    // Embaralhar o array de frases usando o algoritmo de Fisher-Yates
-    for (let i = frases.length - 1; i > 0; i--) {
+function embaralharESortear() {
+  // Embaralhar o array de frases usando o algoritmo de Fisher-Yates
+  for (let i = frases.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [frases[i], frases[j]] = [frases[j], frases[i]];
-    }
-
-    // Sortear frase
-    var indiceSorteado = Math.floor(Math.random() * frases.length);
-    var fraseSorteada = frases[indiceSorteado];
-  
-    // Atualizar os elementos na página com as informações da frase
-    document.getElementById("categoria").textContent = fraseSorteada.categoria;
-    document.getElementById("titulo").textContent = fraseSorteada.titulo;
-    document.getElementById("fraseSorteada").textContent = fraseSorteada.texto;  
   }
-  
-  // Conectar a função de embaralhar ao botão correspondente
-  document.getElementById("embaralharButton").addEventListener("click", embaralharFrases);
+
+  // Sortear frase
+  var indiceSorteado = Math.floor(Math.random() * frases.length);
+  var fraseSorteada = frases[indiceSorteado];
+
+  // Atualizar os elementos na página com as informações da frase
+  document.getElementById("categoria").textContent = fraseSorteada.categoria;
+  document.getElementById("titulo").textContent = fraseSorteada.titulo;
+  document.getElementById("fraseSorteada").textContent = fraseSorteada.texto;
+}
+
+// Conectar a função de embaralhar e sortear ao botão correspondente
+document.getElementById("embaralharSortearButton").addEventListener("click", embaralharESortear);
